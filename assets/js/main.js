adf4f9cbd8166c26fdf70fabb40d8bd102353fa3
Liban Mahad <mahad.l@outlook.be>
2021-01-25 17:16:40 +0100

    Exo boucle

---
+++ assets/js/main.js
@@ -0,0 +1,125 @@
+// let jourSemaine = prompt("Entrez un jour de la semaine")
+// switch(jourSemaine){
+//     case 'lundi' :
+//     console.log( "On est"+" "+jourSemaine+"."+ 'Tu as cours sur le Discord MolenGeek puis tu dois jouer à Among Us');
+//     break ;
+//     case 'mardi' :
+//     console.log("On est"+jourSemaine+"."+ 'Tu as cours sur le Discord MolenGeek puis tu dois jouer à Among Us');
+//     break ;
+//     case 'mercredi' :
+//     console.log( "On est"+jourSemaine+"."+ 'Tu as cours sur le Discord MolenGeek puis tu dois jouer à Among Us');
+//     break ;
+//     case 'jeudi' :
+//     console.log( "On est"+jourSemaine+"."+ 'Tu as cours sur le Discord MolenGeek puis tu dois jouer à Among Us');
+//     break ;
+//     case 'vendredi' :
+//     console.log("On est"+jourSemaine+"."+ 'Tu as cours sur le Discord MolenGeek puis tu dois jouer à Among Us');
+//     break ;
+//     case 'samedi' :
+//     console.log( "On est"+jourSemaine+"."+ 'Tu as cours sur le Discord MolenGeek puis tu dois jouer à Among Us');
+//     break ;
+//     case 'dimache' :
+//     console.log( "On est"+jourSemaine+"."+ 'Tu as cours sur le Discord MolenGeek puis tu dois jouer à Among Us');
+//     break ;
+// default:
+//     console.log("Sorry, je ne comprend pas");
+// }
+
+
+4//
+
+
+// let meteo = prompt("quel temps fait-il aujourd'hui ?")
+// switch (meteo) {
+//     case 'vent':
+//     console.log("il y a du "+ meteo + " hmdl j'ai une veste" );
+//     break;
+//     case 'pluie':
+//     console.log("il y a de la "+ meteo + " hmdl j'ai une veste" );
+//     break;
+//     case 'neige':
+//     console.log("il y a de la "+ meteo + " hmdl j'ai une veste" );
+//     break;
+//     case 'soleil':
+//     console.log("il y a du "+ meteo + " et des nanas" );
+//     break;
+
+
+//     default:
+//         console.log("Tu fais quoi zebi");
+//     break;
+// }
+
+
+3//
+
+// let nombre =parseInt(prompt("donne moi un nombre"))
+// switch (nombre % 2) {
+//     case 0 :
+//         alert(nombre+ " est un nombre pair ")
+//     break;
+    
+//     default:
+//      alert(nombre + " est impair");
+//     break;
+// }
+
+4//
+// let nombre = parseInt(prompt("donne moi un nombre"));
+// let opera = prompt("donne moi une opérateur mathématique");
+// let secondNombre= parseInt(prompt("donne moi un second nombre"));
+
+// switch (opera) {
+//     case "*":
+//     alert(nombre*secondNombre)
+//     break;
+//     case "+":
+//     alert(nombre+secondNombre)
+//     break; 
+//     case "-":
+//     alert(nombre-secondNombre)
+//     break; 
+//     case "/":
+//     alert(nombre/secondNombre)
+//     break; 
+//     default:
+//         alert("a quoi tu joues ? on veut un opé!")
+//         break;
+// }
+
+
+
+// Exo1_boucles-for.md
+//Exo1
+
+
+
+let classe = [ 
+
+     "abdel","amine","antoine","ayoub","bilal","camille","cem", "gauthier","laurie","mahad","mufassa","nghia","redwane","salim","said","yahya","yasmina","yassin"
+     ]
+   for(let i=0; i < classe.length ; i++ )
+    console.log("bonjour "+classe [i]);
+   
+/* exo2 */
+/* let nombre = parseInt(prompt("Donne moi nombre"))
+
+ for(let i=0; i<=nombre; i++ ){
+        console.log(i);
+ }
+
+ */
+/* EXO 2 BOUCLE */
+/* exo1 */
+ /* let classe = [ 
+
+ "abdel","amine","antoine","ayoub","bilal","camille","cem", "gauthier","laurie","mahad","mufassa","nghia","redwane","salim","said","yahya","yasmina","yassin"
+    
+   ]
+
+    classe.forEach(element => console.log("bonjour "+element)); */
+/* exo2 */
+/*  let ele = [
+         "un", "deux","trois","quatre","cinq"
+ ]
+ ele.forEach(element => console.log(element)); */
